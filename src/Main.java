b7112adb1a157329cc7726f2c2e58118dc8653bb
Aditya <adikarve007@gmail.com>
2025-02-28 01:45:53 +0100

    updated

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,9 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello Vistula!");
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
     }
 }
